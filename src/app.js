a1126677f77a5bdc3b0f229d44084a1d789ff6ea
JoseArguinzones <145572262+JoseArguinzones@users.noreply.github.com>
2023-10-28 02:26:07 +0000

    actualizada

--- src/app.js
+++ src/app.js
@@ -1,20 +1,12 @@
-let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon'];
-let i,b,c;
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let i, b, c;
 
-
-
-
-for (i=0;i<pronoun.length;i++){
-    for (b=0;b<adj.length;b++){
-        for (c=0;c<noun.length;c++){
-            console.log(pronoun[i]+adj[b]+noun[c]+".com \n");
-
-        }
+for (i = 0; i < pronoun.length; i++) {
+  for (b = 0; b < adj.length; b++) {
+    for (c = 0; c < noun.length; c++) {
+      console.log(pronoun[i] + adj[b] + noun[c] + ".com \n");
     }
+  }
 }
-
-
-
-
